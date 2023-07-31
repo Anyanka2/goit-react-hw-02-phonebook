26d0a576d9aef0809517c79b2e3d6bdee66473b1
Anyanka2 <123205369+Anyanka2@users.noreply.github.com>
2023-07-31 22:01:24 +0200

    task done

---
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -0,0 +1,42 @@
+import styled from '@emotion/styled';
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 400px;
+  padding: 12px;
+  font-weight: 500;
+  background-color: #dfeff5;
+  border: 3px solid #e88606;
+  border-radius: 8px;
+`;
+export const Input = styled.input`
+  margin-top: 10px;
+  padding: 6px 10px;
+  border-radius: 5px;
+  font: inherit;
+  cursor: pointer;
+`;
+export const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 16px;
+  color: #e88606;
+  font-size: 20px;
+`;
+export const Button = styled.button`
+  padding: 6px 10px;
+  font: inherit;
+  cursor: pointer;
+  border-radius: 8px;
+  border: 3px solid #e88606;
+  color: #e88606;
+  font-size: 20px;
+ 
+  &:hover,
+  &:focus {
+    color: #ffffff;
+    background-color: #e88606;
+    cursor: pointer;
+  }
+`;
